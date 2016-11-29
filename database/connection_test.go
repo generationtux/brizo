43617fdbbfd6406833e7c135ace88aed0b3bcb25
Kyle Ferguson <kylegferg@gmail.com>
2016-11-29 16:23:23 -0500

    setup app config

--- database/connection_test.go
+++ database/connection_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestGetDBAddress(t *testing.T) {
-	os.Setenv("BRIZO_DB_USER", "foo")
-	os.Setenv("BRIZO_DB_PASSWORD", "bar")
-	os.Setenv("BRIZO_DB_HOST", "db.example.com")
-	os.Setenv("BRIZO_DB_PORT", "3307")
-	os.Setenv("BRIZO_DB_NAME", "somedb")
+	os.Setenv("BRIZO_MYSQL_USER", "foo")
+	os.Setenv("BRIZO_MYSQL_PASSWORD", "bar")
+	os.Setenv("BRIZO_MYSQL_HOST", "db.example.com")
+	os.Setenv("BRIZO_MYSQL_PORT", "3307")
+	os.Setenv("BRIZO_MYSQL_DATABASE", "somedb")
 
 	expected := "foo:bar@(db.example.com:3307)/somedb?charset=utf8&parseTime=True&loc=Local"
 	actual := getDBAddress()
@@ -20,11 +20,11 @@ func TestGetDBAddress(t *testing.T) {
 }
 
 func TestGetDBAddressUsesDefaultPort(t *testing.T) {
-	os.Setenv("BRIZO_DB_USER", "foo")
-	os.Setenv("BRIZO_DB_PASSWORD", "bar")
-	os.Setenv("BRIZO_DB_HOST", "db.example.com")
-	os.Setenv("BRIZO_DB_PORT", "")
-	os.Setenv("BRIZO_DB_NAME", "somedb")
+	os.Setenv("BRIZO_MYSQL_USER", "foo")
+	os.Setenv("BRIZO_MYSQL_PASSWORD", "bar")
+	os.Setenv("BRIZO_MYSQL_HOST", "db.example.com")
+	os.Setenv("BRIZO_MYSQL_PORT", "")
+	os.Setenv("BRIZO_MYSQL_DATABASE", "somedb")
 
 	expected := "foo:bar@(db.example.com:3306)/somedb?charset=utf8&parseTime=True&loc=Local"
 	actual := getDBAddress()
