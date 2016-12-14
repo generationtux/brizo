c018949b4cdca353b451d4608f383128215b766e
John Laswell <john.n.laswell@gmail.com>
2016-12-14 10:05:24 -0500

    angular component to view all applications on dashboard page
    Dashboard component now loads in all of the applications on initialization. These are set to a property that can be referenced and injected into the dashboard view. The actual ui portion is just stubbed out so that we can make changes in the future to how it's styled.

--- ui/src/app/dashboard/dashboard.component.ts
+++ ui/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Application } from '../resources/application.component'
 
 @Component({
-    selector:       'dashboard',
-    templateUrl:    './dashboard.component.html'
+    selector:    'dashboard',
+    templateUrl: './dashboard.component.html',
 })
 
-export class DashboardComponent {}
+@Injectable()
+export class DashboardComponent {
+    constructor(private http: Http) {
+      this.getApplications().subscribe(
+        data => this.applications = data,
+        err => this.logError(err),
+        () => console.log('applications loaded')
+      )
+    }
+
+    private applications: Application[]
+
+    private applicationsIndexUrl = '/api/v1/applications'
+
+    logError(err: any) {
+      console.error('There was an error: ' + err);
+    }
+
+    getApplications(): Observable<Application[]> {
+      return this.http.get(this.applicationsIndexUrl)
+        .map((res: Response) => res.json())
+        .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+    }
+}
