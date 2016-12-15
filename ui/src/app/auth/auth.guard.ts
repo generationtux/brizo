efc1a92977d7a21b560dbd77bede766abd1b8373
Lark Mullins <larkmullins@gmail.com>
2016-12-15 17:42:13 -0500

    fixed import spacing and removed commented code

--- ui/src/app/auth/auth.guard.ts
+++ ui/src/app/auth/auth.guard.ts
@@ -1,22 +1,12 @@
-import { Injectable }           from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot }  from '@angular/router';
-import { AuthService }          from './auth.service';
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
 import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
-  /*
-  canActivate() {
-    if(this.auth.validateJwtToken()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
-  }
-  */
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
     if (!this.auth.validateJwtToken()) {
       this.router.navigate(['/login']);
