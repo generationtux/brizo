72591fc96ff86bff8d3eecec6e24698c2fc93371
Kyle Ferguson <kylegferg@gmail.com>
2017-01-12 19:06:53 -0500

    node 6.9

--- ui/config/webpack.prod.js
+++ ui/config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = webpackMerge(commonConfig, {
   devtool: 'source-map',
 
   output: {
-    path: helpers.root('compiled'),
+    path: helpers.root('dist'),
     publicPath: '/assets/',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
