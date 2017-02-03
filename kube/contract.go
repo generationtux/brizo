4ebb269748018c71d32c0ecb62bdcd05916d42c3
Kyle Ferguson <kylegferg@gmail.com>
2017-02-03 11:26:47 -0500

    create and delete deployments

--- kube/contract.go
+++ kube/contract.go
@@ -1,9 +1,15 @@
 package kube
 
+import (
+	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
+)
+
 // APIInterface an interface for interacting with k8s cluster API
 type APIInterface interface {
 	Health() error
-	GetPods(PodOptions) ([]Pod, error)
-	CreateDeployment(*Deployment) error
-	FindDeploymentByName(string) (*Deployment, error)
+	GetPods(string, v1.ListOptions) ([]v1.Pod, error)
+	CreateDeployment(*v1beta1.Deployment) error
+	DeleteDeployment(*v1beta1.Deployment) error
+	FindDeploymentByName(string, string) (*v1beta1.Deployment, error)
 }
--- kube/pods.go
+++ kube/pods.go
@@ -6,101 +6,46 @@ import (
 
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
-	metav1 "k8s.io/client-go/pkg/apis/meta/v1"
 )
 
-// Pod spec
-type Pod struct {
-	Name string
-}
-
-// Deployment spec
-type Deployment struct {
-	Name        string
-	Namespace   string
-	Pods        []Pod
-	MatchLabels map[string]string
-}
-
-// PodOptions used to refine queries for pods
-type PodOptions struct {
-	Namespace string
-	Name      string
-	Labels    string
-}
-
 // GetPods retrieves pods in the k8s cluster
-func (c *Client) GetPods(options PodOptions) ([]Pod, error) {
-	k8sOptions := v1.ListOptions{
-		LabelSelector: options.Labels,
-	}
-
-	podList, err := c.k8sClient.Pods(options.Namespace).List(k8sOptions)
-	pods := make([]Pod, len(podList.Items))
-	for i, pod := range podList.Items {
-		pods[i] = Pod{
-			Name: pod.Name,
-		}
-	}
-
-	return pods, err
+func (c *Client) GetPods(namespace string, options v1.ListOptions) ([]v1.Pod, error) {
+	podList, err := c.k8sClient.Pods(namespace).List(options)
+	return podList.Items, err
 }
 
 // CreateDeployment will create a new deployment in the k8s cluster
 // using the provided deployment spec for configuration
-func (c *Client) CreateDeployment(deployment *Deployment) error {
-	k8sDeployment := new(v1beta1.Deployment)
-	k8sDeployment.Name = deployment.Name
-	k8sDeployment.Namespace = deployment.Namespace
-	k8sDeployment.Spec = v1beta1.DeploymentSpec{
-		Selector: &metav1.LabelSelector{
-			MatchLabels: deployment.MatchLabels,
-		},
-		Template: v1.PodTemplateSpec{},
-	}
-
-	_, err := c.k8sClient.Deployments(deployment.Namespace).Create(k8sDeployment)
+func (c *Client) CreateDeployment(deployment *v1beta1.Deployment) error {
+	_, err := c.k8sClient.Deployments(deployment.Namespace).Create(deployment)
 
 	return err
 }
 
-// FindDeploymentByName will lookup a deployment in the k8s cluster using
-// the provided name
-func (c *Client) FindDeploymentByName(name, namespace string) (*Deployment, error) {
-	k8sDeployment, err := c.k8sClient.Deployments(namespace).Get(name)
-	if err != nil {
-		return new(Deployment), err
-	}
-
-	deployment := new(Deployment)
-	deployment.Name = k8sDeployment.Name
-	deployment.Namespace = k8sDeployment.Namespace
-	deployment.MatchLabels = k8sDeployment.Spec.Selector.MatchLabels
-
-	pods, err := c.getPodsForDeployment(deployment)
-	if err != nil {
-		return new(Deployment), err
-	}
-
-	deployment.Pods = pods
-
-	return deployment, nil
+// DeleteDeployment will delete the provided deployment from k8s
+func (c *Client) DeleteDeployment(deployment *v1beta1.Deployment) error {
+	return c.k8sClient.Deployments(deployment.Namespace).Delete(deployment.Name, &v1.DeleteOptions{})
 }
 
-// getPodsForDeployment will retrieve the pods associated to the provided deployment
-func (c *Client) getPodsForDeployment(deployment *Deployment) ([]Pod, error) {
+// FindDeploymentByName will lookup a deployment in the k8s cluster using
+// the provided name
+func (c *Client) FindDeploymentByName(name, namespace string) (*v1beta1.Deployment, error) {
+	return c.k8sClient.Deployments(namespace).Get(name)
+}
+
+// GetPodsForDeployment will retrieve the pods associated to the provided deployment
+func (c *Client) GetPodsForDeployment(deployment *v1beta1.Deployment) ([]v1.Pod, error) {
 	labelSelector := ""
-	for key, value := range deployment.MatchLabels {
+	for key, value := range deployment.Spec.Selector.MatchLabels {
 		labelSelector = fmt.Sprintf("%s,%s=%s", labelSelector, key, value)
 	}
 	labelSelector = labelSelector[1:] // remove first comma
 
 	if labelSelector == "" {
-		return []Pod{}, errors.New("No label selector specified for deployment")
+		return []v1.Pod{}, errors.New("No label selector specified for deployment")
 	}
 
-	return c.GetPods(PodOptions{
-		Namespace: deployment.Namespace,
-		Labels:    labelSelector,
+	return c.GetPods(deployment.Namespace, v1.ListOptions{
+		LabelSelector: labelSelector,
 	})
 }
