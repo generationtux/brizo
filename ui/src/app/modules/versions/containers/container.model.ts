93f0a0a4314c8f957827506d6885e9b3e0cebd92
Kyle Ferguson <kylegferg@gmail.com>
2017-02-24 10:02:36 -0500

    parse args for containers

--- ui/src/app/modules/versions/containers/container.model.ts
+++ ui/src/app/modules/versions/containers/container.model.ts
@@ -2,6 +2,7 @@ export class Container {
   public name: string;
   public image: string;
   public args: string[];
+  public argString: string;
   public alwaysPull: boolean = true;
   public ports: ContainerPort[];
   public volumeMounts: VolumeMount[];
@@ -11,6 +12,29 @@ export class Container {
     this.args = this.args || [];
     this.ports = this.ports || [];
     this.volumeMounts = this.volumeMounts || [];
+  }
+
+  // convert args string into individual array of args
+  public parseArgString() {
+    if (this.args.length !== 0) {
+      return;
+    }
+
+    let args = this.argString.split(",");
+    let splitNoSpaces = args.map((value) => { return value.trim(); });
+
+    let splitNoQuotes = splitNoSpaces.map((value) => {
+      if (value.substring(0, 1) == '"') {
+        value = value.substring(1);
+      }
+      if (value.substring(value.length-1) == '"') {
+        value = value.substring(0, value.length-1);
+      }
+
+      return value;
+    });
+
+    this.args = splitNoQuotes;
   }
 }
 
