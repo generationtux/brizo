56eda282c3ff441a10734a77edebc0754d2c73fd
Lark Mullins <larkmullins@gmail.com>
2017-03-09 12:11:20 -0500

    fixing indentation

--- database/migrations/8_removeVersionsToEnvironmentsConstraint.go
+++ database/migrations/8_removeVersionsToEnvironmentsConstraint.go
@@ -8,9 +8,9 @@ func removeVersionsToEnvironmentsConstraint() *migrate.Migration {
 	return &migrate.Migration{
 		Id: "8",
 		Up: []string{`
-      ALTER TABLE versions
-        DROP INDEX uix_versions_name_environment_id
-      ;
+	      ALTER TABLE versions
+	        DROP INDEX uix_versions_name_environment_id
+	      ;
     `},
 		Down: []string{``},
 	}
--- database/migrations/9_addApplicationUuidColumnToVersionsTable.go
+++ database/migrations/9_addApplicationUuidColumnToVersionsTable.go
@@ -8,18 +8,18 @@ func addApplicationUUIDColumnToVersionsTable() *migrate.Migration {
 	return &migrate.Migration{
 		Id: "9",
 		Up: []string{`
-      ALTER TABLE versions
-        ADD COLUMN application_uuid varchar(36) NOT NULL AFTER replicas,
-		ADD CONSTRAINT uix_application_uuid UNIQUE (uuid),
-		ADD UNIQUE INDEX uix_versions_name_application_uuid (name, application_uuid)
-      ;
+          ALTER TABLE versions
+            ADD COLUMN application_uuid varchar(36) NOT NULL AFTER replicas,
+    		ADD CONSTRAINT uix_application_uuid UNIQUE (uuid),
+    		ADD UNIQUE INDEX uix_versions_name_application_uuid (name, application_uuid)
+          ;
     `},
 		Down: []string{`
-	  ALTER TABLE versions
-	    DROP CONSTRAINT uix_application_uuid,
-		DROP INDEX uix_versions_name_application_uuid,
-	    DROP COLUMN application_uuid
-	  ;
+    	  ALTER TABLE versions
+    	    DROP CONSTRAINT uix_application_uuid,
+    		DROP INDEX uix_versions_name_application_uuid,
+    	    DROP COLUMN application_uuid
+    	  ;
 	`},
 	}
 }
